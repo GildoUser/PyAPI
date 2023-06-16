23d9cdd050785f371c448ca60779b1ba382ad26a
GildoUser <gildoUser@gmail.com>
2023-06-16 04:16:46 -0300

    Desenvolvendo Get Post e objeto para testes

---
+++ main.py
@@ -0,0 +1,48 @@
+import json
+
+from flask import Flask, send_file, jsonify
+
+app = Flask(__name__)
+
+livros = [{
+    "nome":"Relampago marquinhos",
+    "editora":"num sei cathcau"
+
+},{
+    "nome":"Arrombado de morte",
+    "editora":"Eoem"
+},{
+    "nome":"Cansei ja",
+    "editora":"Quero oportunidade, mas nao estudo"
+},{
+    "nome":"casino clandestino",
+    "editora":"venda de imoveis e jogo do bicho"
+}]
+#
+@app.route("/buscarlivros")
+def get():
+    return jsonify(livros=livros)
+
+@app.route("/inserirlivroeditora=<editora>nome=<nome>")
+def post(editora, nome):
+    for livro in livros:
+        for key,value in livro.items():
+            if key == nome and value == editora:
+                return {"message":"Este livro ja esta cadastrado"}, 409
+
+    obj = {nome: editora}
+    livros.append(obj)
+    print(livros)
+    return "<h1>Funcionou!</h1>", 200
+
+"""@app.route("/eusou<nome>")
+def hello(nome):
+    return f'<h1>Hellooo, {nome}</h1>'"""
+
+@app.route("/favicon.ico")
+def enviaFavIco():
+    return send_file('./assets/icons8-gato.gif', mimetype='image/gif')
+
+
+if __name__ == "__main__":
+    app.run()
